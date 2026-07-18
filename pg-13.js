de73020cd3d200604f4b96acfc1259d96f29de02
Dahichka <202397512+Dahichka@users.noreply.github.com>
2026-07-18 14:23:06 +0300

    fix

--- pg-13.js
+++ pg-13.js
@@ -1,3 +1,11 @@
 var age = 12;
 var accompained = true;
 console.log(age >= 13 || accompained == 1)
+        var name = prompt("What's your name&");
+        console.log("Hello " + name);
+        var words = [
+            "Vasia",
+            "Cat",
+            "Dania"
+        ];
+        var word = words[Math.floor(Math.random()*words.length)];
