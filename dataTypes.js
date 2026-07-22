8af3e33fddbc2102d92a0f45172025b2b24a3b3b
Dahichka <202397512+Dahichka@users.noreply.github.com>
2026-07-22 10:03:56 +0300

    function

--- dataTypes.js
+++ dataTypes.js
@@ -5,3 +5,7 @@ console.log((greeting + name).length);
 console.log(name[0]);
 console.log(name.slice(1));
 console.log(("I am " + name).toUpperCase());
+var randomWords = ["Planet", "Worm", "Flower", "Competer"];
+var pickRandomWord = function (words){
+return words[Math.floor(Math.random()*randomWords.length)];}
+console.log(pickRandomWord(randomWords));
