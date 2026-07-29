b18fef0b521558bbc254bfa6ebeb576b7f8d3c43
Dahichka <202397512+Dahichka@users.noreply.github.com>
2026-07-29 11:31:02 +0300

    py4

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ print("Hello")
 print(5+5)
 x = 2
 print(4+x)
-x = input("Do you like chesse?")
+x = input("Do you like chesse?(y/n)")
 print(x)
+if(x == "y"):
+    print("You cool.")
+
+else:
+    print("ok")
